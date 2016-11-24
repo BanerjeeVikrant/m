fb42d00aabb387ac6d6f4f489e1296ffd00d9448
BanerjeeVikrant <banerjeevikrant2002@gmail.com>
2016-11-23 18:05:33 -0800

    updated

--- action/bringfullscreenpost.php
+++ action/bringfullscreenpost.php
@@ -1,5 +1,6 @@
 <?php
 require "../system/connect.php";
+require '../system/helpers.php';
 
 session_start();
 if (isset($_SESSION['user_login'])) {
@@ -24,9 +25,12 @@ if(isset($_GET['id'])){
 		$date_added = $row['date_added'];
 		$commentsid = $row['commentsid'];
 		$likedby = $row['liked_by'];
+		$commentsid = $row['commentsid'];
 
 		$commentsArray = explode(",",$commentsid);
 		$likedbyArray = explode(",",$likedby);
+
+		$timesincestr = time_elapsed_string($time_added);
 
 		if(in_array($username, $likedbyArray)){
 			$userliked = "<div class = 'like-btn-div'><div id='like-btn-$id' class = 'liked' onclick = 'unlikePost($id);'></div></div>";
@@ -47,46 +51,86 @@ if(isset($_GET['id'])){
 		$lastname = $getU['last_name'];
 		$topName = "<a href = 'profile.php?u=$photoSender' class = 'samepostedby'>$firstname $lastname</a>";
 
+		$commentsArray = [];
+
+		if ($commentsid != "") {
+			$commentsArray = explode(",",$commentsid);
+		}
+        $commentsCount = count($commentsArray);
+        $commentsCountShow = 0;
+        if($commentsCount > 3){
+            $commentsCountShow = $commentsCount - 3;
+        }
+        if($commentsCountShow != 0){
+        $commentShownBox = "<div style = 'position: relative;' class='view-more'>                      
+                <div class = 'comment-body'>
+                    <div class = 'comments-img'></div>
+                    <div class = 'comment-area'>
+                        <div style = 'position: relative;'>
+                            <div class = 'commentPosted'>
+                                &nbsp;&nbsp;&nbsp;View $commentsCountShow more comments
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>";
+        }
+        else{
+            $commentShownBox = "";
+        }
+        $defaultCommentsCount = 3;
 		echo "
-		<div class='topbar-userimages'>
-			<div class='back-img' id='back-photos'></div>
-			<div class='userimages-name'>$firstname's Post</div>
-		</div>
-		<div class = 'profile-post'>
-			<div style = 'position: relative;'>
-				<div class = 'glyphicon glyphicon-option-vertical post-options'>
-					<div class = 'postoptions-div' style = 'display: none; position: absolute;top: 19px;left:-113px;background-color: #F3F3F3;width: 126px;height: 90px;'>
-
-						<div class = 'hide-post post-work' style = ''> <span class = 'glyphicon glyphicon-lock'></span> Hide</div>
-						<div class = 'delete-post post-work' style = ''> <span class = 'glyphicon glyphicon-remove'></span> Delete</div>
-						<div class = 'report-post post-work' style = ''> <span class = 'glyphicon glyphicon-flag'></span> Report</div>
-					</div>
-				</div>
+			<div class='topbar-userimages'>
+				<div class='back-img' id='back-photos'></div>
+				<div class='userimages-name'>$firstname's Post</div>
 			</div>
-			<div class = 'posted-by-img' style = 'background-image: url($userpic);'></div>
-			<span class = 'topName'>
-				$topName<br>
-				<span class = 'time'>$time_added<span>, </span>$date_added</span>
-			</span>
-			<hr class='post-breaker'>
-			<p class = 'msg-body'>$body</p>
-			$pic
-		</div>
-		<textarea style = 'display: none;' id = 'comments-send'></textarea>
-		<div class = 'comments-input'>
-			$userliked
-			<div style = 'position: relative;'>
-				<form method = 'POST' class='post-comment'>
-					<input type = 'text' name = 'comment' placeholder = 'Write a Comment&hellip;' class = 'comment-inputs' autocomplete = 'off' />
-					<input type = 'text' name = 'id' value = '$id' style = 'display: none;' />
-					<input type = 'submit' id = 'commentid' name = 'commentid' style = 'display: none;'/>        
-				</form>        
-			</div>        
-		</div>
-		<div class = 'comments-box'>
-			";
+            <div id = 'profile-post-$id' style='display:inline-block;'>
+			<div class = 'profile-post' homeid='$id'>
+				<div style = 'position: relative;'>
+					<div class = 'glyphicon glyphicon-option-vertical post-options' id='$id'></div>
+				</div>
+				<div class = 'posted-by-img' style = 'background-image: url($userpic);'></div>
+				<span class = 'topName'>
+					$topName<br>
+					<span class = 'time'>$timesincestr</span>
+				</span>
+                <hr class='post-breaker'>
+				<p class = 'msg-body'>$body</p>
+				$pic
+			</div>
+            <textarea style = 'display: none;' id = 'comments-send'></textarea>
+            <div class = 'comments-input'>
+                    $userliked
+                <div style = 'position: relative;'>
+                    <form method = 'POST' class='post-comment'>
+                        <input type = 'text' name = 'comment' placeholder = 'Write a Comment&hellip;' class = 'comment-inputs' autocomplete = 'off' />
+                        <input type = 'text' name = 'id' value = '$id' style = 'display: none;' />
+                        <input type = 'submit' id = 'commentid' name = 'commentid' style = 'display: none;'/>        
+                    </form>        
+                </div>        
+            </div>
+			<div class = 'comments-box'>
 
-				foreach ($commentsArray as $value) {
+
+                <!--PEOPLE WHO LIKED-->
+
+
+                <div class = 'comment-body likers_$id' onclick='show_likers_$id()' style='font-size:13px;padding-left:10px;padding-bottom:10px;padding-top:10px'></div>
+
+                <script>
+                posturl = 'action/getlikers.php?id=$id';
+                $.ajax({url: posturl, success: function(result){
+                        $('.likers_$id').html(result);
+                    }
+                });
+                </script>
+
+
+                $commentShownBox
+                <div class = 'old-comment-box'>";
+
+				for ($i = 0; $i < $commentsCount - $defaultCommentsCount; $i++) {
+                    $value = $commentsArray[$i];
 					$getCommentQuery = $conn->query("SELECT * FROM comments WHERE id='$value' LIMIT 1");
 					$getCommentRow = $getCommentQuery->fetch_assoc();
 					$commentPost = $getCommentRow['comment'];
@@ -94,7 +138,7 @@ if(isset($_GET['id'])){
 					$getUser = $conn->query("SELECT * FROM users WHERE username = '$commentpostedby'");
 					$getfetch = $getUser->fetch_assoc();
 					$userpic = $getfetch['profile_pic'];
-					echo "		
+					echo "                
 					<div style = 'position: relative;'>                        
 						<div class = 'comment-body'>
 							<div class = 'comments-img'></div>
@@ -107,9 +151,42 @@ if(isset($_GET['id'])){
 							</div>
 						</div>
 					</div>
-			
+
 					";
 				}
+                echo "
+                </div>
+                ";
+                for ($i = max(0,$commentsCount - $defaultCommentsCount); $i < $commentsCount; $i++) {
+                    $value = $commentsArray[$i];
+                    $getCommentQuery = $conn->query("SELECT * FROM comments WHERE id='$value' LIMIT 1");
+                    $getCommentRow = $getCommentQuery->fetch_assoc();
+                    $commentPost = $getCommentRow['comment'];
+                    $commentpostedby =  $getCommentRow['from'];
+                    $getUser = $conn->query("SELECT * FROM users WHERE username = '$commentpostedby'");
+                    $getfetch = $getUser->fetch_assoc();
+                    $userpic = $getfetch['profile_pic'];
+                    echo "                
+                    <div style = 'position: relative;'>                        
+                        <div class = 'comment-body'>
+                            <div class = 'comments-img'></div>
+                            <div class = 'comment-area'>
+                                <div style = 'position: relative;'>
+                                    <div class = 'commentPosted'>
+                                        <a style='position: relative;' href = 'profile.php?u=$commentpostedby'>$commentpostedby</a>&nbsp;&nbsp;&nbsp;$commentPost
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    ";
+                }
+
+				echo "        
+			</div>
+            </div>
+			";
 
 			echo "        
 		</div>
@@ -123,3 +200,41 @@ if(isset($_GET['id'])){
 }
 
 ?>
+		<script>
+
+			$('.post-comment').submit(function(e){
+				e.preventDefault();
+				var curr_position = $(this).closest('.post-comment');
+				postcomment(curr_position);
+				e.unbind();
+			});
+
+            var boxOpen = false;
+
+			function openOptions(postid){
+                var newElem="";
+                newElem += "<div class='optionBox-wrapper'><div class='optionBox' pid='"+postid+"'>";
+                newElem += "    <div class='optionsPost' id='deletepost' onclick='deletepost("+postid+");'>Delete<\/div>";
+                newElem += "    <div class='optionsPost' id='reportpost' onclick='reportpost("+postid+");'>Report<\/div>";
+                newElem += "<\/div><\/div>";
+
+                if(boxOpen == false){
+                    $("#anyreport").prepend(newElem);
+                    boxOpen = true;
+                }
+            }
+
+
+			$(".post-options").click(function() {
+                var postid = $(this).attr("id");
+                openOptions(postid);
+			});
+
+            $(".old-comment-box").hide();
+
+            $(".view-more").click(function(){
+                $(this).next().show();
+                $(this).hide();
+            });
+
+		</script>
