4e913f80c43ae47b107948b88dbdf047cacb3403
BanerjeeVikrant <banerjeevikrant2002@gmail.com>
2016-12-26 22:31:19 -0800

    d

--- action/bringposts.php
+++ action/bringposts.php
@@ -363,40 +363,7 @@ function identifyTagsInMsg($msg) {
 
                         $commentShownBox
                         <div class = 'old-comment-box'>";
-
-    					for ($i = 0; $i < $commentsCount - $defaultCommentsCount; $i++) {
-                            echo "<h1>Inside it works here</h1>";
-                            $value = $commentsArray[$i];
-    						$getCommentQuery = $conn->query("SELECT * FROM comments WHERE id='$value' LIMIT 1");
-    						$getCommentRow = $getCommentQuery->fetch_assoc();
-    						$commentPost = $getCommentRow['comment'];
-    						$commentpostedby =  $getCommentRow['from'];
-    						$getUser = $conn->query("SELECT * FROM users WHERE id = '$commentpostedby'");
-    						$getfetch = $getUser->fetch_assoc();
-    						$userpic = $getfetch['profile_pic'];
-                            $commentpostedbyusr = $getfetch['username'];
-                            echo "<h1>It also works here</h1>";
-    						echo "                
-    						<div style = 'position: relative;'>                        
-    							<div class = 'comment-body'>
-    								<div class = 'comments-img'></div>
-    								<div class = 'comment-area'>
-    									<div style = 'position: relative;'>
-    										<div class = 'commentPosted'>
-    											<a style='position: relative;' href = 'profile.php?u=$commentpostedbyusr'>$commentpostedbyusr</a>&nbsp;&nbsp;&nbsp;$commentPost
-    										</div>
-    									</div>
-    								</div>
-    							</div>
-    						</div>
-
-    						";
-    					}
-                        echo "
-                        </div>
-                        ";
-                        for ($i = max(0,$commentsCount - $defaultCommentsCount); $i < $commentsCount; $i++) {
-                            echo "<h1>It also also works here</h1>";
+                        for ($i = 0; $i < $commentsCount - $defaultCommentsCount; $i++) {
                             $value = $commentsArray[$i];
                             $getCommentQuery = $conn->query("SELECT * FROM comments WHERE id='$value' LIMIT 1");
                             $getCommentRow = $getCommentQuery->fetch_assoc();
@@ -405,8 +372,7 @@ function identifyTagsInMsg($msg) {
                             $getUser = $conn->query("SELECT * FROM users WHERE id = '$commentpostedby'");
                             $getfetch = $getUser->fetch_assoc();
                             $userpic = $getfetch['profile_pic'];
-                            $commentpostedbyusr = $getfetch['username'];
-                            echo "<h1>It also also also works here</h1>";
+                            $usr = $getfetch['username'];
                             echo "                
                             <div style = 'position: relative;'>                        
                                 <div class = 'comment-body'>
@@ -414,15 +380,43 @@ function identifyTagsInMsg($msg) {
                                     <div class = 'comment-area'>
                                         <div style = 'position: relative;'>
                                             <div class = 'commentPosted'>
-                                                <a style='position: relative;' href = 'profile.php?u=$commentpostedbyusr'>$commentpostedbyusr</a>&nbsp;&nbsp;&nbsp;$commentPost
+                                                <a style='position: relative;' href = 'profile.php?u=$usr'>$usr</a>&nbsp;&nbsp;&nbsp;$commentPost
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-
                             ";
                         }
+                        echo "
+                        </div>
+                        ";
+                        for ($i = max(0,$commentsCount - $defaultCommentsCount); $i < $commentsCount; $i++) {
+                            $value = $commentsArray[$i];
+                            $getCommentQuery = $conn->query("SELECT * FROM comments WHERE id='$value' LIMIT 1");
+                            $getCommentRow = $getCommentQuery->fetch_assoc();
+                            $commentPost = $getCommentRow['comment'];
+                            $commentpostedby =  $getCommentRow['from'];
+                            $getUser = $conn->query("SELECT * FROM users WHERE id = '$commentpostedby'");
+                            $getfetch = $getUser->fetch_assoc();
+                            $userpic = $getfetch['profile_pic'];
+                            $usr = $getfetch['username'];
+                            echo "                
+                            <div style = 'position: relative;'>                        
+                                <div class = 'comment-body'>
+                                    <div class = 'comments-img'></div>
+                                    <div class = 'comment-area'>
+                                        <div style = 'position: relative;'>
+                                            <div class = 'commentPosted'>
+                                                <a style='position: relative;' href = 'profile.php?u=$usr'>$usr</a>&nbsp;&nbsp;&nbsp;$commentPost
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            ";
+                        }
+
 
     					echo "        
     				</div>
