feb41be1bba6b9b171912da1c1fb89ad039da036
BanerjeeVikrant <banerjeevikrant2002@gmail.com>
2016-12-29 12:58:03 -0800

    d

--- test.php
+++ test.php
@@ -1,7 +1,21 @@
-<h1>
+
 
 <?php
-echo $_GET['data'];
-?>
+echo "hi1";
 
-</h1>
+$ch = curl_init();
+
+echo "hi2";
+
+curl_setopt($ch, CURLOPT_URL,"https://pushify.com/api/authenticate");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "email=vikrantbanerjee5@gmail.com&pass=VB123yoyo1D!");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec ($ch);
+
+echo "h3"
+//echo "<script>alert(".$server_output.");";
+
+curl_close ($ch);
+?>
