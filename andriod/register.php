c8c1a2d8eb01f497a973758f18d27e5bf4e436de
BanerjeeVikrant <banerjeevikrant2002@gmail.com>
2017-01-01 17:22:46 -0800

    d

--- andriod/register.php
+++ andriod/register.php
@@ -11,7 +11,21 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    
+
+    $ip = "";
+
+    // Without a proper internet server $ip is not going to work
+
+    if ($_SERVER['HTTP_CLIENT_IP']!="") {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } 
+    elseif ($_SERVER['HTTP_X_FORWARDED_FOR']!="") {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+
     $fn = strip_tags(@$_POST['fn']);
     $ln = strip_tags(@$_POST['ln']);
     $usr = strip_tags(@$_POST['usr']);
@@ -19,24 +33,15 @@
     $pswd = strip_tags(@$_POST['psw']);
     $pswd2 = strip_tags(@$_POST['psw2']);
     $dob = strip_tags(@$_POST['dob']);
-    $gender = strip_tags(@$_POST['gender']);
-    if($gender == "Male"){
-        $gender = 1;
-    }
-    else if($gender == "Female"){
-        $gender = 2;
-    }
-
     $grade = strip_tags(@$_POST['grade']);
     $stuid = strip_tags(@$_POST['stuid']);
-    $num = strip_tags(@$_POST['num']);
 
     $d = date("m/d/y");
     $t = time();
 
 
 
-    $statement = $conn->query("INSERT INTO users VALUES ('', '$usr', '$fn', '$ln', '$stuid', '$pswdmd5', '$d', '', '', '', '', '', '$gender', '$dob', '0', '', '', '', '', '0', '$grade', '$ip', '$ip', '$ip', '1', '$d', '$t', '0', '1', '', '')");
+    $statement = $conn->query("INSERT INTO users VALUES ('', '$usr', '$fn', '$ln', '$stuid', '$pswd', '$d', '', '', '', '', '', '', '$dob', '0', '', '', '', '', '0', '$grade', '$ip', '$ip', '$ip', '1', '$d', '$t', '0', '1', '', '')");
 
     
     $response = array();
