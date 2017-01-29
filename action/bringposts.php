4416c8ea278ae5cc3a4c39148201853c408391ce
BanerjeeVikrant <banerjeevikrant2002@gmail.com>
2017-01-28 17:39:25 -0800

    d

--- action/bringposts.php
+++ action/bringposts.php
@@ -101,7 +101,7 @@ if (!isset($_GET['u'])) {
 	$yourfollowing_arr =  explode(',',$yourfollowing);
 	$yourfollowing_quoted = "'".implode("','",$yourfollowing_arr)."'";
     if ($group == 0) {
-    	$sql = "SELECT * FROM posts WHERE ((added_by IN ($yourfollowing_quoted) AND posted_to = '0') OR (added_by = '$usernameid' AND posted_to = '0') AND (post_group = '0')) ORDER BY id DESC LIMIT $offset,20";
+    	$sql = "SELECT * FROM posts WHERE ((added_by IN ($yourfollowing_quoted) AND posted_to = '0') OR (added_by = '$usernameid' AND posted_to = '0' AND post_group = '0') AND (post_group = '0')) ORDER BY id DESC LIMIT $offset,20";
     }
     else {
         $sql = "SELECT * FROM posts WHERE (post_group = '$group') ORDER BY id DESC LIMIT $offset,20";
