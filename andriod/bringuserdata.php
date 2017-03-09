76d8fa0c1d3790f77aa7e2af13e9297d2752c994
BanerjeeVikrant <banerjeevikrant2002@gmail.com>
2017-03-08 20:32:10 -0800

    d

--- andriod/bringuserdata.php
+++ andriod/bringuserdata.php
@@ -13,40 +13,38 @@
     include "../system/helpers.php";
 
     $username = $_POST['user'];
-    //$profileUser = $_GET['puser'];
+    $you = $_POST['puser'];
 
-/*
-    if ($profileUser) {
-        $check = $conn->query("SELECT * FROM users WHERE username='$profileUser'");
-        if ($check->num_rows == 1) {
 
-            $get = $check->fetch_assoc();
-            $id = $get['id'];
-            $firstname = $get['first_name'];
-            $lastname = $get['last_name'];
-            $profilepic = $get['profile_pic'];
-            $bannerpic = $get['bannerimg'];
-            $followers = $get['followers'];        
-            $following = $get['following'];
-            $sex = $get['sex'];
-            if($profilepic == "" || $profilepic == NULL){
-                if($sex == "1"){
-                    $profilepic = "https://upload.wikimedia.org/wikipedia/commons/3/34/PICA.jpg";
-                }
-                else{
-                    $profilepic = "http://www4.csudh.edu/Assets/CSUDH-Sites/History/images/Faculty-Profile-Pictures/Faculty%20Female%20Default%20Profile%20Picture.jpg";
-                }
-            }else{
-                $profilepic = "http://www.bruincave.com/m/" . $profilepic;
+    $check = $conn->query("SELECT * FROM users WHERE username='$you'");
+    if ($check->num_rows == 1) {
+
+        $get = $check->fetch_assoc();
+        $id = $get['id'];
+        $firstname = $get['first_name'];
+        $lastname = $get['last_name'];
+        $profilepic = $get['profile_pic'];
+        $bannerpic = $get['bannerimg'];
+        $followers = $get['followers'];        
+        $following = $get['following'];
+        $sex = $get['sex'];
+        if($profilepic == "" || $profilepic == NULL){
+            if($sex == "1"){
+                $profilepic = "https://upload.wikimedia.org/wikipedia/commons/3/34/PICA.jpg";
             }
-        }
-        if($bannerpic == "" || $bannerpic == NULL){
-            
+            else{
+                $profilepic = "http://www4.csudh.edu/Assets/CSUDH-Sites/History/images/Faculty-Profile-Pictures/Faculty%20Female%20Default%20Profile%20Picture.jpg";
+            }
         }else{
-            $bannerpic = "http://www.bruincave.com/m/" . $bannerpic;
+            $profilepic = "http://www.bruincave.com/m/" . $profilepic;
         }
     }
-    */
+    if($bannerpic == "" || $bannerpic == NULL){
+        
+    }else{
+        $bannerpic = "http://www.bruincave.com/m/" . $bannerpic;
+    }
+    
     $checkme = $conn->query("SELECT * FROM users WHERE username='$username'");
     if ($checkme->num_rows == 1) {
 
@@ -60,6 +58,17 @@
         $yourfollowing = $getuser['following'];
         $yourbio = $getuser['bio'];
         $yoursex = $getuser['sex'];
+
+        $yourfollowers_array = explode(",", $yourfollowers);
+
+        if($id == $yourid){
+            $followCase = 0;
+        } else if(in_array($id, $yourfollowers_array)){
+            $followCase = 1;
+        } else{
+            $followCase = 2;
+        }
+
         if($yourprofilepic == "" || $yourprofilepic == NULL){
             if($yoursex == "1"){
                 $yourprofilepic = "https://upload.wikimedia.org/wikipedia/commons/3/34/PICA.jpg";
@@ -88,7 +97,8 @@
                     "bio": "'.$yourbio.'",
                     "bannerpic": "'.$yourbannerpic.'",
                     "userpic": "'.$yourprofilepic.'",
-                    "name": "'.$yourfirstname. " " .$yourlastname.'"
+                    "name": "'.$yourfirstname. " " .$yourlastname.'",
+                    "followcase":'.$followCase.'
                 }  
     ]
 }
