0f2188093e089969e847b65c323a31da5bfe9baf
BanerjeeVikrant <banerjeevikrant2002@gmail.com>
2017-04-15 21:59:02 -0700

    d

--- andriod/photoupload.php
+++ andriod/photoupload.php
@@ -7,7 +7,9 @@ $dbname = "bruincaveData";
 function base64_to_jpeg($base64_string, $output_file) {
     $ifp = fopen($output_file, "w"); 
 
-    fwrite($ifp, base64_decode($base64_string)); 
+    $data = explode(',', $base64_string);
+
+    fwrite($ifp, base64_decode($data[1])); 
     fclose($ifp); 
 
     return $output_file; 
