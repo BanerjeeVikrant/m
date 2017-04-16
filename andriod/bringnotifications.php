84c0bf56c10f5996afd91893871260a4970c4716
BanerjeeVikrant <banerjeevikrant2002@gmail.com>
2017-04-16 14:01:19 -0700

    d

--- andriod/bringnotifications.php
+++ andriod/bringnotifications.php
@@ -69,6 +69,7 @@ $getposts = $conn->query($sql) or die(mysql_error());
                     "id":'.$id.',
                     "body": "'.$message.'",
                     "fromPic": "'.$fromPic.'",
+                    "postid":'.$postId.',
                     "fromFirst": "'.$fromFirst.'",
                     "from_user":"'.$fromUser_urname.'",
                     "time_added":"'.$notifierTime.'"
@@ -81,6 +82,7 @@ $getposts = $conn->query($sql) or die(mysql_error());
                     "id":'.$id.',
                     "body": "'.$message.'",
                     "fromPic": "'.$fromPic.'",
+                    "postid":'.$postId.',
                     "fromFirst": "'.$fromFirst.'",
                     "from_user":"'.$fromUser_urname.'",
                     "time_added":"'.$notifierTime.'"
