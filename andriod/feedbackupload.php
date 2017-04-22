f173d5cbb009db69832bd222d82841a777bd56a1
BanerjeeVikrant <banerjeevikrant2002@gmail.com>
2017-04-21 21:08:23 -0700

    d

---
+++ andriod/feedbackupload.php
@@ -0,0 +1,57 @@
+<?php
+$servername = "localhost";
+$username1 = "root";
+$password = "H@ll054321";
+$dbname = "bruincaveData";
+
+// Create connection
+$conn = new mysqli($servername, $username1, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$username = $_POST['u'];
+$results = $conn->query("SELECT * FROM users WHERE username='$username'");
+$rowget = $results->fetch_assoc();
+$usernameid = $rowget['id'];
+$usernamegrade = $rowget['grade'];
+
+$post = $_POST['caption'];
+
+if(isset($_POST['image'])){
+	$now = DateTime::createFromFormat('U.u', microtime(true));
+	$id = $now->format('YmdHisu');
+	date_default_timezone_set("America/Los_Angeles");
+	$date_added = date("Y/m/d");
+	$added_by = $username;
+	$time_added = time();
+
+	$upload_folder = "../userdata/pictures/$username";
+	if (!file_exists("../userdata/pictures/$username")){
+		mkdir("../userdata/pictures/$username");
+		mkdir("../userdata/pictures/$username/thumbnail");
+	}
+	$path = "../userdata/pictures/$username/$id.jpg";
+	$image = $_POST['image'];
+
+	$ext='jpeg';
+    $data = base64_decode( $image );
+
+    file_put_contents($path, $data );
+	
+	$sql = "INSERT INTO feedback_table VALUES ('', 'usernameid', '', 'userdata/pictures/$username/$id.jpg')";
+
+	if ($conn->query($sql) === TRUE) {
+		$response["success"] = true;  
+		echo json_encode($response);
+	}else{
+		$response["success"] = false;  
+		echo json_encode($response);
+	}
+
+}else{
+	echo "image_not_in";
+	exit;
+}
+
+?>
