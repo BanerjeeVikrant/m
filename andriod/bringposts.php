e915bea37df910384da695ec155bf281a87c65a8
BanerjeeVikrant <banerjeevikrant2002@gmail.com>
2017-05-26 23:24:05 -0700

    d

--- andriod/bringposts.php
+++ andriod/bringposts.php
@@ -186,9 +186,7 @@
                     $get_comment = $comment->fetch_assoc();
 
                     $body = $get_comment['comment'];
-                    $body = str_replace("&apos;","'",$body);
-                    $body = str_replace("&lt;","<",$body);
-                    $body = str_replace("&gt;",">",$body);
+                    
                     $from_ = $get_comment['from'];
 
                     $query = $conn->query("SELECT * FROM users WHERE id='$from_'");
