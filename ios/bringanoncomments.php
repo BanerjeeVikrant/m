16817dd5ceec8d76916bafca33ab67dcedd84fc9
BanerjeeVikrant <banerjeevikrant2002@gmail.com>
2017-12-02 21:59:19 -0800

    changes

--- ios/bringanoncomments.php
+++ ios/bringanoncomments.php
@@ -51,7 +51,7 @@
                     if ($body != "") {
 
                         $commentsArr .= "
-                                        ,{
+                                        {
                                           'body':'$body'
                                         }";
                     }
