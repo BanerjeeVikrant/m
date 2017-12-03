6e080ccd5c5ea2b7ce9bb80a7dd4b5836bc5ceae
BanerjeeVikrant <banerjeevikrant2002@gmail.com>
2017-12-02 21:56:21 -0800

    changes

---
+++ ios/bringanoncomments.php
@@ -0,0 +1,68 @@
+<?php
+    $servername = "localhost";
+    $username1 = "root";
+    $password = "H@ll054321";
+    $dbname = "bruincaveData";
+
+    // Create connection
+    $conn = new mysqli($servername, $username1, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    include "../system/helpers.php";
+
+    $postid = $_POST['postid'];
+
+    $sql =  "SELECT * FROM crush WHERE id='$postid'";
+
+    $getposts = $conn->query($sql) or die(mysql_error());
+
+    if($getposts->num_rows > 0) {
+        echo '
+{
+    "crush": [';
+        $row = $getposts->fetch_assoc();
+            $id = $row['id'];
+            $body = $row['body'];
+            $commentsid = $row['commentsid'];
+            $commentsid_array = explode(",", $commentsid);
+
+            
+                echo '
+                { 
+                    "body": "'.$body.'"
+                }';  
+
+            $commentsArr = "";
+
+            foreach ($commentsid_array as $value) {
+                if ($value != '') {
+                    $comment = $conn->query("SELECT * FROM anoncomments WHERE id='$value'");
+
+                    $get_comment = $comment->fetch_assoc();
+
+                    $body = $get_comment['comment'];
+
+
+                    if ($commentsArr != "") {
+                        $commentsArr = $commentsArr.",";
+                    }
+                    if ($body != "") {
+
+                        $commentsArr .= "
+                                        ,{
+                                          'body':'$body'
+                                        }";
+                    }
+                 }
+            }
+            echo $commentsArr; 
+    }
+        
+ echo "
+    ]}
+";
+    
+
+?>
