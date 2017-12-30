10effab6d99c8d03daebd75d3f0ebc1123974a05
BanerjeeVikrant <banerjeevikrant2002@gmail.com>
2017-12-29 17:51:43 -0800

    save

---
+++ ios/postcrush.php
@@ -0,0 +1,40 @@
+<?php
+    $servername = "localhost";
+    $username1 = "root";
+    $password = "H@ll054321";
+    $dbname = "bruincaveData";
+
+    // Create connection
+    $conn = new mysqli($servername, $username1, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    include "../system/helpers.php";
+
+    $post = $_POST['post'];
+    $post = str_replace("'","&apos;",$post);
+    $post = str_replace("<","&lt;",$post);
+    $post = str_replace(">","&gt;",$post);
+    $username = $_POST['u'];
+
+    $checkme = $conn->query("SELECT * FROM users WHERE username='$username'");
+    if ($checkme->num_rows == 1) {
+        $getuser = $checkme->fetch_assoc();
+        $usernameid = $getuser['id'];
+    }
+ 
+    if($post != ""){
+        date_default_timezone_set("America/Los_Angeles");
+        $date_added = '';
+        $time_added = time(); 
+        $added_by = $username;
+
+        $sqlcommand = "INSERT INTO crush VALUES ('', '$post', '$usernameid', '', '$time_added', '$date_added','')";
+        $query = $conn->query($sqlcommand);
+    }
+
+    $success = true; 
+    echo '{"info": [{"successError":'.$success.'}]}'; 
+
+?>
