eaed4344a14882d0a0c992f0fc4065ece66dd099
BanerjeeVikrant <banerjeevikrant2002@gmail.com>
2018-01-07 22:03:15 -0800

    save

--- android/login.php
+++ android/login.php
@@ -1,5 +1,15 @@
 <?php
-    require "/connect.php";
+    $servername = "localhost";
+    $username1 = "root";
+    $password = "VB2002yoyo1D";
+    $dbname = "test";
+
+    // Create connection
+    $conn = new mysqli($servername, $username1, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
     $ip = "";
 
