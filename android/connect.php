ccdb8e70adbd8281af48e54a8c5a0a3070ef81c0
BanerjeeVikrant <banerjeevikrant2002@gmail.com>
2018-01-07 21:58:46 -0800

    save

--- android/connect.php
+++ android/connect.php
@@ -3,4 +3,11 @@
 	$username1 = "root";
 	$password = "VB2002yoyo1D";
 	$dbname = "test";
+
+	// Create connection
+    $conn = new mysqli($servername, $username1, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 ?>
